308b2ba15bfff856d4a7a923089dd3039b6ace80
Michael Zemler <michael.zemler@hotmail.com>
2019-11-09 13:52:47 +0100

    fixed horizontal and vertical scrolling bugs

--- app.js
+++ app.js
@@ -4,18 +4,17 @@ const navSlide = () => {
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
     const body = document.querySelector("body");
-    const navOverlay = document.querySelector(".nav-overlay");
+    //const navOverlay = document.querySelector(".nav-overlay");
     burger.addEventListener("click", () => {
       nav.classList.toggle("nav-active");
-      navOverlay.classList.toggle("nav-open");
+      //navOverlay.classList.toggle("nav-open");
 
       navLinks.forEach((link, index) => {
         if (link.style.animation) {
           link.style.animation = "";
         } else {
           link.style.animation = link.style.animation = `navLinkFade 0.5s ease forwards ${index /
-            7 +
-            0.1}s`;
+            9}s`;
         }
         console.log(index / 7);
       });
